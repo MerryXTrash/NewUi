c6c895950d13ab9796bdf68a73781802a68d8413
MerryXTrash <158755815+MerryXTrash@users.noreply.github.com>
2024-02-17 23:45:36 +0700

    Update TrashX.lua

--- TrashX.lua
+++ TrashX.lua
@@ -2,7 +2,6 @@ _G.Color = Color3.fromRGB(0, 0, 255)
 _G.Logo = 16236291528
 local Win = library:Evil("Evo","Merry HUB",_G.Logo )
 
-
 local Tab = Win:CraftTab('Main')
 local Page = Tab:CraftPage('Main',1)
 Page:Button('Button',function() --Name
