2073c085ccec220d1ea6799e46f3b02195ad2bef
MerryXTrash <158755815+MerryXTrash@users.noreply.github.com>
2024-02-17 23:49:39 +0700

    Update TrashX.lua

--- TrashX.lua
+++ TrashX.lua
@@ -1,5 +1,6 @@
 _G.Color = Color3.fromRGB(0, 0, 255)
 _G.Logo = 16236291528
+
 local normal = loadstring(game:HttpGet(('https://raw.githubusercontent.com/Lucifer4381/ui-normal-hub/main/scr')))()
 
 local Win = library:Evil("Evo","Merry HUB",_G.Logo )
