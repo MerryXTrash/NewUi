ef6c2c2f507ad4078c406e5e6c382716e7926e00
MerryXTrash <158755815+MerryXTrash@users.noreply.github.com>
2024-02-17 23:38:20 +0700

    Update TrashX.lua

--- TrashX.lua
+++ TrashX.lua
@@ -5,7 +5,7 @@ _G.Logo = 16236291528
 local Evil = loadstring(game:HttpGet("https://raw.githubusercontent.com/lntanon55/Golf-HUB/main/Protected_9405524596134885.lua.txt"))()
 local Win = library:Evil("Merry Hub","",_G.Logo )
 
-local tab1 = Win:CraftTab('Angela Hub')
+local tab1 = Win:CraftTab('Merry Hub')
 local page1 = tab1:CraftPage('Main',1)
 
 local player = page1:Label('Label')
